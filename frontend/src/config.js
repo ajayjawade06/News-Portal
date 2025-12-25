56199cb55f121c4fc67f874cda79289e2766bed6
ajujawade <ajayjawade06@gmail.com>
2025-12-26 02:02:53 +0530

    added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,6 @@
 // API Configuration
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'http://localhost:5000';
+// export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'|| ;
+// export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'http://localhost:5000'|'https://news-portal-tau-nine.vercel.app';
 
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://news-portal-tau-nine.vercel.app';
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://news-portal-tau-nine.vercel.app';
