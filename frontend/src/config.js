982871536010ec0f03202ebc620e4650e4dfa8fd
ajujawade <ajayjawade06@gmail.com>
2025-12-26 02:06:06 +0530

    added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,5 +2,5 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'|| ;
 // export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'http://localhost:5000'|'https://news-portal-tau-nine.vercel.app';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://news-portal-tau-nine.vercel.app';
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://news-portal-tau-nine.vercel.app';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://news-portal-production.up.railway.app/';
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://news-portal-production.up.railway.app/';
