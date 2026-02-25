192d564230a10e359ec284da316152b8077a96c7
ajujawade <ajayjawade06@gmail.com>
2026-02-26 02:01:39 +0530

    fixed cors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // API Configuration
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://https://news-portal-9gtx.onrender.com';
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://https://news-portal-9gtx.onrender.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://news-portal-9gtx.onrender.com';
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://news-portal-9gtx.onrender.com';
