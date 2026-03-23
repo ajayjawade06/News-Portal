e0a8af2cd5729f460dc7c030eed7f259c38afced
ajujawade <ajayjawade06@gmail.com>
2026-03-23 23:31:40 +0530

    changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
-export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || '';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://news-portal-9gtx.onrender.com';
+export const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL || 'https://news-portal-9gtx.onrender.com';
